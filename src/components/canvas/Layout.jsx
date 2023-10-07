da2e253af65273923f6c0b505d408453eaed5f91
hakan <hbilgic1992@gmail.com>
2023-10-07 20:25:27 +0200

    next r3f starter

---
+++ src/components/canvas/Layout.jsx
@@ -0,0 +1,34 @@
+'use client'
+
+import dynamic from "next/dynamic";
+import { useRef } from "react";
+const Scene = dynamic(() => import("@/components/canvas/Scene"), {
+  ssr: false,
+});
+
+export const Layout = ({ children }) => {
+  const ref = useRef();
+  return (
+    <div ref={ref} style={{
+      position: 'relative',
+      width: ' 100%',
+      height: '100%',
+      overflow: 'auto',
+      touchAction: 'auto',
+    }}>
+      {children}
+      <Scene
+        eventSource={ref}
+        eventPrefix="client"
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          pointerEvents: 'none',
+        }}
+      />
+    </div>
+  );
+};
---
+++ src/components/helpers/Three.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+import tunnel from 'tunnel-rat'
+
+export const r3f = tunnel()
+
+export const Three = ({ children }) => {
+  return <r3f.In>{children}</r3f.In>
+}
+
